43af7da8c63a36f0b84b6570455748ccc286fa1e
rtbe <kondr.egserg@gmail.com>
2023-06-17 18:34:01 +0500

    refactor: rename gorilla path parser back

--- pkg/gorilla/path.go
+++ pkg/gorilla/path.go
@@ -7,8 +7,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// NewPath returns new path parser for mux router.
-func NewPath(r *http.Request, name string) (string, bool) {
+// Path path parser for mux router.
+func Path(r *http.Request, name string) (string, bool) {
 	vars := mux.Vars(r)
 	path, exists := vars[name]
 	if !exists {
