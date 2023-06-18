f9ac7441a4afb2e2776737e8036c34fbc12ff2a1
rtbe <kondr.egserg@gmail.com>
2023-06-18 19:36:47 +0500

    tests: make subtests for integer_test.go

--- value/integer_test.go
+++ value/integer_test.go
@@ -8,275 +8,277 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSetIntegerString(t *testing.T) {
-	var testStruct struct {
-		S string
-	}
+func TestSetInteger(t *testing.T) {
+	t.Run("String", func(t *testing.T) {
+		var testStruct struct {
+			S string
+		}
 
-	v := reflect.Indirect(reflect.ValueOf(&testStruct))
+		v := reflect.Indirect(reflect.ValueOf(&testStruct))
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int8(num))
-		require.NoError(t, err)
-		require.Equal(t, strconv.Itoa(num), fieldValue.String())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int8(num))
+			require.NoError(t, err)
+			require.Equal(t, strconv.Itoa(num), testStruct.S)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int16(num))
-		require.NoError(t, err)
-		require.Equal(t, strconv.Itoa(num), fieldValue.String())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int16(num))
+			require.NoError(t, err)
+			require.Equal(t, strconv.Itoa(num), testStruct.S)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int32(num))
-		require.NoError(t, err)
-		require.Equal(t, strconv.Itoa(num), fieldValue.String())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int32(num))
+			require.NoError(t, err)
+			require.Equal(t, strconv.Itoa(num), testStruct.S)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int64(num))
-		require.NoError(t, err)
-		require.Equal(t, strconv.Itoa(num), fieldValue.String())
-	}
-}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int64(num))
+			require.NoError(t, err)
+			require.Equal(t, strconv.Itoa(num), testStruct.S)
+		}
+	})
 
-func TestSetIntegerBool(t *testing.T) {
-	var testStruct struct {
-		B bool
-	}
+	t.Run("Bool", func(t *testing.T) {
+		var testStruct struct {
+			B bool
+		}
 
-	v := reflect.Indirect(reflect.ValueOf(&testStruct))
+		v := reflect.Indirect(reflect.ValueOf(&testStruct))
 
-	for i := 0; i < v.NumField(); i++ {
-		num := 1
+		for i := 0; i < v.NumField(); i++ {
+			num := 1
 
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int8(num))
-		require.NoError(t, err)
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int8(num))
+			require.NoError(t, err)
 
-		require.Equal(t, num > 0, fieldValue.Bool())
-	}
+			require.Equal(t, num > 0, testStruct.B)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		num := 0
+		for i := 0; i < v.NumField(); i++ {
+			num := 0
 
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int8(num))
-		require.NoError(t, err)
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int8(num))
+			require.NoError(t, err)
 
-		require.Equal(t, num > 0, fieldValue.Bool())
-	}
+			require.Equal(t, num > 0, testStruct.B)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		num := 1
+		for i := 0; i < v.NumField(); i++ {
+			num := 1
 
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int16(num))
-		require.NoError(t, err)
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int16(num))
+			require.NoError(t, err)
 
-		require.Equal(t, num > 0, fieldValue.Bool())
-	}
+			require.Equal(t, num > 0, testStruct.B)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		num := 0
+		for i := 0; i < v.NumField(); i++ {
+			num := 0
 
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int16(num))
-		require.NoError(t, err)
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int16(num))
+			require.NoError(t, err)
 
-		require.Equal(t, num > 0, fieldValue.Bool())
-	}
+			require.Equal(t, num > 0, testStruct.B)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		num := 1
+		for i := 0; i < v.NumField(); i++ {
+			num := 1
 
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int32(num))
-		require.NoError(t, err)
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int32(num))
+			require.NoError(t, err)
 
-		require.Equal(t, num > 0, fieldValue.Bool())
-	}
+			require.Equal(t, num > 0, testStruct.B)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		num := 0
+		for i := 0; i < v.NumField(); i++ {
+			num := 0
 
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int32(num))
-		require.NoError(t, err)
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int32(num))
+			require.NoError(t, err)
 
-		require.Equal(t, num > 0, fieldValue.Bool())
-	}
+			require.Equal(t, num > 0, testStruct.B)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		num := 1
+		for i := 0; i < v.NumField(); i++ {
+			num := 1
 
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int64(num))
-		require.NoError(t, err)
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int64(num))
+			require.NoError(t, err)
 
-		require.Equal(t, num > 0, fieldValue.Bool())
-	}
+			require.Equal(t, num > 0, testStruct.B)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		num := 0
+		for i := 0; i < v.NumField(); i++ {
+			num := 0
 
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int64(num))
-		require.NoError(t, err)
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int64(num))
+			require.NoError(t, err)
 
-		require.Equal(t, num > 0, fieldValue.Bool())
-	}
-}
+			require.Equal(t, num > 0, testStruct.B)
+		}
+	})
 
-func TestSetIntegerInt(t *testing.T) {
-	var testStruct struct {
-		I   int
-		I8  int8
-		I16 int64
-		I32 int32
-		I64 int64
-	}
+	t.Run("Int", func(t *testing.T) {
+		var testStruct struct {
+			I   int
+			I8  int8
+			I16 int64
+			I32 int32
+			I64 int64
+		}
 
-	v := reflect.Indirect(reflect.ValueOf(&testStruct))
+		v := reflect.Indirect(reflect.ValueOf(&testStruct))
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int8(num))
-		require.NoError(t, err)
-		require.Equal(t, int64(num), fieldValue.Int())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int8(num))
+			require.NoError(t, err)
+			require.Equal(t, int64(num), fieldValue.Int())
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int16(num))
-		require.NoError(t, err)
-		require.Equal(t, int64(num), fieldValue.Int())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int16(num))
+			require.NoError(t, err)
+			require.Equal(t, int64(num), fieldValue.Int())
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int32(num))
-		require.NoError(t, err)
-		require.Equal(t, int64(num), fieldValue.Int())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int32(num))
+			require.NoError(t, err)
+			require.Equal(t, int64(num), fieldValue.Int())
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int64(num))
-		require.NoError(t, err)
-		require.Equal(t, int64(num), fieldValue.Int())
-	}
-}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int64(num))
+			require.NoError(t, err)
+			require.Equal(t, int64(num), fieldValue.Int())
+		}
+	})
 
-func TestSetIntegerUint(t *testing.T) {
-	var testStruct struct {
-		I   uint
-		I8  uint8
-		I16 uint64
-		I32 uint32
-		I64 uint64
-	}
+	t.Run("Uint", func(t *testing.T) {
+		var testStruct struct {
+			I   uint
+			I8  uint8
+			I16 uint64
+			I32 uint32
+			I64 uint64
+		}
 
-	v := reflect.Indirect(reflect.ValueOf(&testStruct))
+		v := reflect.Indirect(reflect.ValueOf(&testStruct))
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int8(num))
-		require.NoError(t, err)
-		require.Equal(t, uint64(num), fieldValue.Uint())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int8(num))
+			require.NoError(t, err)
+			require.Equal(t, uint64(num), fieldValue.Uint())
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int16(num))
-		require.NoError(t, err)
-		require.Equal(t, uint64(num), fieldValue.Uint())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int16(num))
+			require.NoError(t, err)
+			require.Equal(t, uint64(num), fieldValue.Uint())
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int32(num))
-		require.NoError(t, err)
-		require.Equal(t, uint64(num), fieldValue.Uint())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int32(num))
+			require.NoError(t, err)
+			require.Equal(t, uint64(num), fieldValue.Uint())
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int64(num))
-		require.NoError(t, err)
-		require.Equal(t, uint64(num), fieldValue.Uint())
-	}
-}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int64(num))
+			require.NoError(t, err)
+			require.Equal(t, uint64(num), fieldValue.Uint())
+		}
+	})
 
-func TestSetIntegerFloat(t *testing.T) {
-	var testStruct struct {
-		FL32 float32
-		FL64 float64
-	}
+	t.Run("Float", func(t *testing.T) {
+		var testStruct struct {
+			FL32 float32
+			FL64 float64
+		}
 
-	v := reflect.Indirect(reflect.ValueOf(&testStruct))
+		v := reflect.Indirect(reflect.ValueOf(&testStruct))
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int8(num))
-		require.NoError(t, err)
-		require.Equal(t, float64(num), fieldValue.Float())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int8(num))
+			require.NoError(t, err)
+			require.Equal(t, float64(num), fieldValue.Float())
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int16(num))
-		require.NoError(t, err)
-		require.Equal(t, float64(num), fieldValue.Float())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int16(num))
+			require.NoError(t, err)
+			require.Equal(t, float64(num), fieldValue.Float())
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int32(num))
-		require.NoError(t, err)
-		require.Equal(t, float64(num), fieldValue.Float())
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int32(num))
+			require.NoError(t, err)
+			require.Equal(t, float64(num), fieldValue.Float())
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int64(num))
-		require.NoError(t, err)
-		require.Equal(t, float64(num), fieldValue.Float())
-	}
-}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int64(num))
+			require.NoError(t, err)
+			require.Equal(t, float64(num), fieldValue.Float())
+		}
+	})
 
-func TestSetIntegerUnsupportedType(t *testing.T) {
-	var testStruct struct {
-		SL []string
-	}
+	t.Run("Unsupported", func(t *testing.T) {
+		var testStruct struct {
+			SL []string
+		}
 
-	v := reflect.Indirect(reflect.ValueOf(&testStruct))
+		v := reflect.Indirect(reflect.ValueOf(&testStruct))
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int8(num))
-		require.Error(t, err)
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int8(num))
+			require.Error(t, err)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int16(num))
-		require.Error(t, err)
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int16(num))
+			require.Error(t, err)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int32(num))
-		require.Error(t, err)
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int32(num))
+			require.Error(t, err)
+		}
 
-	for i := 0; i < v.NumField(); i++ {
-		fieldValue := v.Field(i)
-		err := SetInteger(&fieldValue, int64(num))
-		require.Error(t, err)
-	}
+		for i := 0; i < v.NumField(); i++ {
+			fieldValue := v.Field(i)
+			err := SetInteger(&fieldValue, int64(num))
+			require.Error(t, err)
+		}
+	})
 }
