8b2c1fc037f95ae50a29350993dbc2348f437268
Kirill Malikov <slipros@gmail.com>
2025-01-03 20:06:14 +0300

    add: router lookup for httprouter path parser (#42)

--- pkg/httprouter/path.go
+++ pkg/httprouter/path.go
@@ -17,3 +17,24 @@ func Path(r *http.Request, name string) (string, bool) {
 
 	return path, true
 }
+
+// NewPath returns new path parser for httprouter router.
+func NewPath(router *httprouter.Router) func(r *http.Request, name string) (string, bool) {
+	return func(r *http.Request, name string) (string, bool) {
+		if router == nil {
+			return "", false
+		}
+
+		_, params, _ := router.Lookup(r.Method, r.URL.Path)
+		if len(params) == 0 {
+			return "", false
+		}
+
+		path := params.ByName(name)
+		if len(path) == 0 {
+			return "", false
+		}
+
+		return path, true
+	}
+}
