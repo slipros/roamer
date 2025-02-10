89da5ed47d2a45e5d64dc56fb39ebbfe67df34a5
Kirill Malikov <slipros@gmail.com>
2025-02-10 19:36:02 +0300

    fix: set strings slice to slice of strings enumerations (#47)

--- value/slice.go
+++ value/slice.go
@@ -8,28 +8,47 @@ import (
 	rerr "github.com/slipros/roamer/err"
 )
 
-var typeSliceOfAny = reflect.TypeOf([]any{})
+var (
+	typeAnySlice    = reflect.TypeOf([]any{})
+	typeStringSlice = reflect.TypeOf([]string{})
+)
 
 // SetSliceString sets slice of strings into a field.
 func SetSliceString(field reflect.Value, arr []string) error {
+	fieldType := field.Type()
 	switch field.Kind() {
 	case reflect.String:
 		field.SetString(strings.Join(arr, ","))
+
 		return nil
 	case reflect.Slice:
-		elemKind := field.Type().Elem().Kind()
-		switch elemKind {
+		elemType := fieldType.Elem()
+		switch elemType.Kind() {
 		case reflect.String:
+			if fieldType != typeStringSlice && typeString.ConvertibleTo(elemType) {
+				slice := reflect.MakeSlice(fieldType, 0, len(arr))
+				for _, v := range arr {
+					casted := reflect.ValueOf(v).Convert(elemType)
+					slice = reflect.Append(slice, casted)
+				}
+
+				field.Set(slice)
+
+				return nil
+			}
+
 			field.Set(reflect.ValueOf(arr))
+
 			return nil
 		case reflect.Interface:
-			if field.Type().AssignableTo(typeSliceOfAny) {
+			if field.Type().AssignableTo(typeAnySlice) {
 				s := make([]any, 0, len(arr))
 				for _, v := range arr {
 					s = append(s, v)
 				}
 
 				field.Set(reflect.ValueOf(s))
+
 				return nil
 			}
 		}
@@ -39,6 +58,7 @@ func SetSliceString(field reflect.Value, arr []string) error {
 		switch field.Interface().(type) {
 		case []string:
 			field.Set(reflect.ValueOf(arr))
+
 			return nil
 		}
 	}
